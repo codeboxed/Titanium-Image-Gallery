835832b7332e139a0fb26364fd3388809f3df2bd
Codeboxed <team@codeboxed.com>
2011-07-18 10:50:34 +0100

    Some small path fixes for the example project

--- Resources/image_gallery_module.js
+++ Resources/image_gallery_module.js
@@ -3,12 +3,12 @@ var ImageGallery = require('com.codeboxed.imagegallery');
 var win = Ti.UI.currentWindow;
 
 var imagesArray = [
-	{path:'1.jpg', caption:'Kitten 1'},
-	{path:'2.jpg', caption:'Kitten 2'},
-	{path:'3.jpg', caption:'Kitten 3'},
-	{path:'4.jpg', caption:'Kitten 4'},
-	{path:'5.jpg', caption:'Kitten 5'},
-	{path:'6.jpg', caption:'Kitten 6'}
+	{path:'images/1.jpg', caption:'Kitten 1'},
+	{path:'images/2.jpg', caption:'Kitten 2'},
+	{path:'images/3.jpg', caption:'Kitten 3'},
+	{path:'images/4.jpg', caption:'Kitten 4'},
+	{path:'images/5.jpg', caption:'Kitten 5'},
+	{path:'images/6.jpg', caption:'Kitten 6'}
 ];
 
 var imageGallery = ImageGallery.create({
